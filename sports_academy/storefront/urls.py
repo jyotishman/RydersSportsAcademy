f1d2f29525468e4af367de572031ec3e85f21d41
Shubham Agrawal <shubham.agrawal@timesinternet.in>
2019-02-07 00:50:26 +0530

    [add] Add storefront pages

--- sports_academy/storefront/urls.py
+++ sports_academy/storefront/urls.py
@@ -4,4 +4,11 @@ from . import views
 
 urlpatterns = [
     url(r'^$', views.HomeView.as_view(), name='home'),
+    url(r'^centers/$', views.CenterView.as_view(), name='centers-list'),
+    url(r'^centers/(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.CenterDetailView.as_view(), name='centers-detail'),
+    url(r'^gallery/$', views.GalleryView.as_view(), name='galleries'),
+    url(r'^sports/$', views.SportView.as_view(), name='sports'),
+    url(r'^centers/(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.SportDetailView.as_view(), name='sports-detail'),
+    url(r'^centers/(?P<pk>[0-9]+)/sports/$', views.SportCenterView.as_view(), name='sports'),
+    url(r'^teams/$', views.TeamView.as_view(), name='teams'),
 ]
--- sports_academy/storefront/views.py
+++ sports_academy/storefront/views.py
@@ -1,16 +1,106 @@
-from rest_framework.views import View
 from django.shortcuts import render
+from rest_framework.views import View
+from sports_academy.center.models import Center
+from sports_academy.sport.models import Sport
+from django.shortcuts import get_object_or_404
 
 # Create your views here.
 
 
 class HomeView(View):
-    template_name = "home.html"
+	template_name = "home.html"
 
-    def get(self, request, *args, **kwargs):
-        context = {
-            'meta_title': "Showtop10- The best 10 list of everything",
-            'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
-            'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
-        }
-        return render(request, self.template_name, context=context)
+	def get(self, request, *args, **kwargs):
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class GalleryView(View):
+	template_name = "gallery.html"
+
+	def get(self, request, *args, **kwargs):
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class CenterView(View):
+	template_name = "centers.html"
+
+	def get(self, request, *args, **kwargs):
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class CenterDetailView(View):
+	template_name = "center-detail.html"
+
+	def get(self, request, pk=None, *args, **kwargs):
+		center = get_object_or_404(Center, pk=pk)
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class SportView(View):
+	template_name = "sports.html"
+
+	def get(self, request, *args, **kwargs):
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class SportDetailView(View):
+	template_name = "sport-detail.html"
+
+	def get(self, request, pk=None, *args, **kwargs):
+		sport = get_object_or_404(Sport, pk=pk)
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class SportCenterView(View):
+	template_name = "sports-centers.html"
+
+	def get(self, request, *args, **kwargs):
+		sport = get_object_or_404(Sport, pk=pk)
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
+
+
+class TeamView(View):
+	template_name = "teams.html"
+
+	def get(self, request, *args, **kwargs):
+		context = {
+			'meta_title': "Showtop10- The best 10 list of everything",
+			'meta_description': "Top 10 list of everything and anything in one place. Get the best ten list everyday.",
+			'image': "https://d14nytznni7htl.cloudfront.net/standalone/17663/og_image_1542134794_7567792.png"
+		}
+		return render(request, self.template_name, context=context)
