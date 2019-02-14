e5ee1320d6f96ccb27d2ad913ad6841719dd8221
jyotishman <jyotishman82@gmail.com>
2019-02-14 07:14:03 +0530

    ui changes

--- app/scripts/sport-city-selection.js
+++ app/scripts/sport-city-selection.js
@@ -1,31 +1,43 @@
 export class SportCitySelection {
-	constructor(){
-		document.querySelector('.selected-sport').addEventListener('click', () => {
-			this.toggleSportSelection();
-		});
+    constructor() {
+        document.querySelector('.selected-sport').addEventListener('click', () => {
+            this.toggleSportSelection();
+        });
 
-		document.querySelector('.selected-center').addEventListener('click', () => {
-			this.toggleCitySelection();
-		});
-		var el = document.querySelectorAll('.custom-select li');
-		for(var i=0; i < el.length; i++){
-		    el[i].addEventListener('click',(e) => {
-		        this.showSelected(e);
-		    }, false);
-		}
-		
-	}
-	toggleClass(el, className) {
-		el.classList.toggle(className)
-	}
-	toggleSportSelection() {
-		this.toggleClass(document.querySelector('.selected-sport'), 'active');
-	}
-	toggleCitySelection() {
-		this.toggleClass(document.querySelector('.selected-center'), 'active');
-	}
-	showSelected(el) {
-		document.getElementsByClassName("selected-sport").innerHTML(el.target.innerHTML)
-	}
+        document.querySelector('.selected-center').addEventListener('click', () => {
+            this.toggleCitySelection();
+        });
+        var el = document.querySelectorAll('.selected-sport + .custom-select li');
+        for (var i = 0; i < el.length; i++) {
+            el[i].addEventListener('click', (e) => {
+                this.showSportSelected(e);
+            }, false);
+        }
+
+        var el = document.querySelectorAll('.selected-center  + .custom-select li');
+        for (var i = 0; i < el.length; i++) {
+            el[i].addEventListener('click', (e) => {
+                this.showCenterSelected(e);
+            }, false);
+        }
+
+    }
+    toggleClass(el, className) {
+        el.classList.toggle(className)
+    }
+    toggleSportSelection() {
+        this.toggleClass(document.querySelector('.selected-sport'), 'active');
+    }
+    toggleCitySelection() {
+        this.toggleClass(document.querySelector('.selected-center'), 'active');
+    }
+    showSportSelected(el) {
+        document.getElementsByClassName("selected-sport")[0].innerHTML = el.target.innerHTML;
+        document.getElementsByClassName("selected-sport")[0].classList.remove('active');
+    }
+    showCenterSelected(el) {
+        document.getElementsByClassName("selected-center")[0].innerHTML = el.target.innerHTML;
+        document.getElementsByClassName("selected-center")[0].classList.remove('active');
+    }
 
 }
