98089e3cee7bae5f68b30e29cff3eef7f1253044
jyotishman <jyotishman82@gmail.com>
2019-02-16 00:19:36 +0530

    slider added

--- app/scripts/index.js
+++ app/scripts/index.js
@@ -1,9 +1,11 @@
 import {SportCitySelection} from "./sport-city-selection";
+import {Slider} from "./slider";
 
 export default class App {
 	constructor(){
 		this.init();
 		this.sports = new SportCitySelection();
+		this.slider = new Slider();
 		
 	}
 	init() {
---
+++ app/scripts/slider.js
@@ -0,0 +1,9 @@
+import {tns} from '../bower_components/tiny-slider/dist/tiny-slider.js';
+export class Slider {
+	constructor(){
+		
+		document.addEventListener("DOMContentLoaded", function(){
+		  
+		});
+	}
+}
