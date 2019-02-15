b7c49259f0204e7d2c33500d88828ef7a10f3efc
Shubham Agrawal <shubham.agrawal@timesinternet.in>
2019-02-16 01:07:20 +0530

    [add] Validation on company

--- config/v1_urls.py
+++ config/v1_urls.py
@@ -2,6 +2,7 @@ from django.conf.urls import include, url
 from rest_framework import routers
 
 from sports_academy.center import views as center_views
+from sports_academy.company import views as company_views
 from sports_academy.gallery import views as gallery_views
 from sports_academy.sport import views as sport_views
 from sports_academy.storefront import views as storefront_views
@@ -9,6 +10,7 @@ from sports_academy.team import views as team_views
 
 router = routers.DefaultRouter()
 router.register(r'center', center_views.CenterViewSet, base_name='centers')
+router.register(r'company', company_views.CompanyViewSet, base_name='company')
 router.register(r'gallery', gallery_views.GalleryViewSet, base_name='galleries')
 router.register(r'contact-us', storefront_views.ContactUsViewSet, base_name='contact-us')
 router.register(r'sport', sport_views.SportViewSet, base_name='sports')
--- sports_academy/company/serializers.py
+++ sports_academy/company/serializers.py
@@ -6,4 +6,5 @@ from . import models
 class CompanySerializer(serializers.ModelSerializer):
 	class Meta:
 		model = models.Company
-		fields = ('id', 'name', 'twitter_url', 'facebook_url', 'instagram_url', 'google_plus_url', 'linkedin_url', 'address', 'city', 'state', 'country', 'zipcode', 'created', 'modified')
+		fields = ('id', 'name', 'twitter_url', 'facebook_url', 'instagram_url', 'google_plus_url', 'linkedin_url',
+		          'address', 'city', 'state', 'country', 'zipcode', 'created', 'modified')
