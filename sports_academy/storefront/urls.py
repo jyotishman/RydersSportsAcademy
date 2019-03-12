ee472e5f008699eccbd37a3cda939ae386ccd35f
jyotishman <jyotishman82@gmail.com>
2019-03-13 01:01:52 +0530

    url

--- sports_academy/storefront/urls.py
+++ sports_academy/storefront/urls.py
@@ -26,7 +26,7 @@ urlpatterns = sitemap_urlpatterns + [
     url(r'^corporates/$', views.CorporateView.as_view(), name='corporate-view-list'),
     url(r'^visitors/$', views.VisitorsView.as_view(), name='visitors-view-list'),
     url(r'^reviews/$', views.ReviewsView.as_view(), name='reviews-view-list'),
-    url(r'^media/$', views.NewsView.as_view(), name='news-view-list'),
+    url(r'^news/$', views.NewsView.as_view(), name='news-view-list'),
     url(r'^notification/(?P<pk>[0-9]+)/(?P<slug>[\w-]+)/$', views.NotificationDetailView.as_view(),
         name='notification-view-detail'),
 ]
