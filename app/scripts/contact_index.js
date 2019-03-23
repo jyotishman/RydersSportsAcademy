5108da00ef45b58cfba5c3d3930f53c02252f353
jyotishman <jyotishman82@gmail.com>
2019-03-23 13:20:49 +0530

    image fix

--- app/scripts/contact_index.js
+++ app/scripts/contact_index.js
@@ -2,9 +2,18 @@ import {Contact} from "./contact-form";
 
 export default class App {
 	constructor(){
-		
+		this.init();
 		this.contact = new Contact();
 		
+	}
+	init() {
+		let bLazy = new Blazy({
+		  offset: 0,
+		  selector: 'img',
+		  loadInvisible: false,
+		  success: function() {
+		  }
+		});
 	}
 	
 }
--- app/scripts/gallery_index.js
+++ app/scripts/gallery_index.js
@@ -2,12 +2,18 @@ import {Gallery} from "./gallery";
 
 export default class App {
 	constructor(){
-		
+		this.init();
 		this.gallery = new Gallery();
 		
 	}
 	init() {
-
+		let bLazy = new Blazy({
+		  offset: 0,
+		  selector: 'img',
+		  loadInvisible: false,
+		  success: function() {
+		  }
+		});
 	}
 }
 const app = new App();
